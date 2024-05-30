e8abfb8174969d1883ad661b17b33f2423878d81
Robin Schmidt <robin@rs-met.com>
2024-05-30 22:53:47 +0200

    Update PlotExamples.py

--- Snippets/PlotExamples.py
+++ Snippets/PlotExamples.py
@@ -1,33 +1,23 @@
 # A simple Plot taken from here:
-# https://matplotlib.org/stable/gallery/lines_bars_and_markers/simple_plot.html#sphx-glr-gallery-lines-bars-and-markers-simple-plot-py    
+# https://matplotlib.org/stable/gallery/lines_bars_and_markers/simple_plot.html#sphx-glr-gallery-lines-bars-and-markers-simple-plot-py
+# with some tweaks applied to make it look better
 
-# Import libraries
+# Import libraries:
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Configure libraries:
-# Set up the style sheet:
 plt.style.use('dark_background')
 #plt.style.use('default')
 #plt.style.use('grayscale')
-
-# See: https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
-
-# To define my own style sheet:
-# https://matplotlib.org/stable/users/explain/customizing.html#customizing-with-style-sheets
-
-    
-
 
 # Create data for plotting:
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
-
-# Create a plot and sho
+# Create a plot and show it:
 px = 1/plt.rcParams['figure.dpi']  # pixel in inches w
 fig, ax = plt.subplots(figsize=(800*px, 400*px))
-#plt.subplots(figsize=(600*px, 200*px))
 ax.plot(t, s)
 ax.set(xlabel='x-axis', ylabel='y-axis', title='The sine function')
 ax.grid()
@@ -44,9 +34,17 @@ plt.show()
 # sucks!
 
 
+#------------------------------------------------------------------------------
+#
+# See: https://matplotlib.org/stable/gallery/style_sheets/style_sheets_reference.html
+#
+# To define my own style sheet:
+# https://matplotlib.org/stable/users/explain/customizing.html#customizing-with-style-sheets
+#
+#    
 # To set up colors:
 # https://stackoverflow.com/questions/14088687/how-to-change-plot-background-color
 # https://matplotlib.org/stable/gallery/style_sheets/dark_background.html
-
+#
 # Some simple example plots:
 # https://matplotlib.org/stable/gallery/pyplots/index.html
