e3b1462c0da5f7367101cbb403698068607ccb6e
Robin Schmidt <robin@rs-met.com>
2024-05-31 16:16:13 +0200

    added VanDerPolSystem.py

--- Projects/Sketch/SketchPad.py
+++ Projects/Sketch/SketchPad.py
@@ -1,13 +1,13 @@
-# We generate and plot the phase space trajectory and time series of the Van 
-# der Pol system defined by the (autonomous) system of ordinary differential 
-# equations:
+# We generate and plot the phase space trajectory and time series of the Lorenz 
+# system defined by the (autonomous) system of ordinary differential equations:
 #
-#   x' = a * y
-#   y' = b * (1 - x^2) * y - x
+#   x' = a * (y - x)
+#   y' = b*x - x*z - y
+#   z' = x*y - c*z
 #
-# where x = x(t) and y = y(t) are two coupled variables that change with 
-# respect to time and x' = dx/dt, y' = dy/dt are their derivatives with respect 
-# to time. The system quickly settles into a (quasi?) periodic motion.
+# where x = x(t), y = y(t), z = z(t) are three coupled variables that change 
+# with respect to time and x' = dx/dt, y' = dy/dt, z' = dz/dt are their 
+# derivatives with respect to time. The system behaves chaotically ...TBC...
 
 # Import libraries:
 import numpy as np                    # For array data types
@@ -18,36 +18,45 @@ from scipy.integrate import odeint    # The numerical ODE solver
 plt.style.use('dark_background')      # I really like dark mode!
 
 # Define system parameters:
-a = 2.2 
-b = 2.0
+a = 10
+b = 28
+c = 8/3.0
 # Using higher values for both increases the frequency and also seems to make 
 # the x-curve steeper and the y-curve more spikey.
 
 # Define initial conditions. We start close to zero:
-x0 = +0.001                           # x(0)
-y0 = -0.001                           # y(0)
+x0 = 0                                # x(0)
+y0 = 1                                # y(0)
+z0 = 0                                # z(0)
 
 # Define length of signal:
-tMax = 30.0                           # Is the unit seconds? I guess so.
+tMax = 80.0                           # Is the unit seconds? I guess so.
+N    = 10001                          # Numer of samples
 
 # Define the function to compute the derivative of the Van der Pol system:
 def f(v, t):
-    xd = a * v[1]                     # x' = a * y
-    yd = b * (1-v[0]**2)*v[1]-v[0]    # y' = b * (1 - x^2)*y - x
-    return np.array([xd, yd])
+    x  = v[0]
+    y  = v[1]
+    z  = v[2]
+    xd = a * (y-x)                     # x' = ...
+    yd = b*x - x*z - y                 # y' = ...
+    zd = x*y - c*z                     # z' = ...
+    return np.array([xd, yd, zd])
 
 # Produce the data for plotting. The solution vt = v(t) is a vector-valued 
 # time series:
-t     = np.linspace(0.0, tMax, 1001)  # Time axis
-v0    = np.array([x0, y0])            # Initial conditions
-vt    = odeint(f, v0, t)              # Solution of the ODE
-(x,y) = vt.T                          # Extract x(t), y(t) from vector v(t)
+t       = np.linspace(0.0, tMax, N)    # Time axis
+v0      = np.array([x0, y0, z0])       # Initial conditions
+vt      = odeint(f, v0, t)             # Solution of the ODE
+(x,y,z) = vt.T                         # Extract x(t), y(t) from vector v(t)
 
 # Plot the trajectory in phase space:
 fig1 = plt.figure()
-plt.plot(x, y)
+plt.plot(x, z)
+# ToDo: Try to create a 3D plot
 
 # Plot the two time series x(t), y(t):
 fig2 = plt.figure()
 plt.plot(t, x, color='lightsalmon')
 plt.plot(t, y, color='skyblue')
+plt.plot(t, z, color='wheat')
