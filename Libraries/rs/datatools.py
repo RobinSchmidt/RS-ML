92603a7a6f1a02ab398b158e4970197f860b9a4e
Robin Schmidt <robin@rs-met.com>
2024-06-01 22:10:35 +0200

    Update datatools.py

--- Libraries/rs/datatools.py
+++ Libraries/rs/datatools.py
@@ -16,12 +16,33 @@ def signal_ar_to_nn(s, d):
             X[n-D, k] = s[n - d[k]]
     return X, y    
     
-    
-    # for n in range(0, N-D):
-    #     y[n] = s[n+D]
-    #     for k in range(0, M):
-    #         X[n, k] = s[n + d[k]]
-    # return X, y
+# NEEDS UNIT TESTS!
+# Example:
+# d = [3,5]
+# s = [1,2,3,4,5,6,7,8,9]
+# y = [6,7,8,9]
+# X = [[3,1],[4,2],[5,3],[6,4]]
 
-            
+#==============================================================================
+# Unit Tests:
+
+def test_datatools():
+    ok = True
+    
+    # Test signal_ar_to_nn:
+    d  = [3,5]                                        # Array of delay times
+    s  = [1,2,3,4,5,6,7,8,9]                          # Input signal
+    Xt = np.array([[3.,1.],[4.,2.],[5.,3.],[6.,4.]])  # Target for X
+    yt = np.array([6.,7.,8.,9.])                      # Target for y
+    X, y = signal_ar_to_nn(s, d) 
+    ok &= X == Xt
+    #ok &= y == yt
+    
+    return ok
+
+if __name__ == "__main__":
+    ok = test_datatools()
+    assert(ok)
+
+    
 
