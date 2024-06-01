a51bbbfa18ef69bb12682099c5a0a4613747fdc6
Robin Schmidt <robin@rs-met.com>
2024-06-01 18:03:14 +0200

    Update dynsys.py

--- Libraries/rs/dynsys.py
+++ Libraries/rs/dynsys.py
@@ -1,5 +1,20 @@
 '''
-This module contains...TBC...
+DynSys: Dynamical Systems
+=========================
+
+This module contains functions that implement the equations of various 
+continuous nonlinear dynamical systems such as the Lorenz system, Van der Pol 
+oscillator, Volterra-Lotka model etc. The functions have all the same signature 
+and can be called like:
+
+    derivs = f(state, time, ...)
+  
+where "state" is the current state vector of the system, time is a scalar time 
+variable and after that, a bunch of parameters for the particular system may 
+follow. The signature of the functions is compatible with the odeint routine 
+from scipy.integrate which implements a numerical ODE solver. Functions that
+implement the equations of autonomous systems will ignore the "time" parameter.
+It is still there as dummy parameter for the compatibility with odeint, though.
 '''
 
 import numpy as np
@@ -13,8 +28,6 @@ def van_der_pol(v, t, mu):
         
       x' = y                                   \n
       y' = mu * (1 - x^2) * y - x
-      
-    The function can be used with scipy.integrate.odeint.
 
     Parameters
     ----------
@@ -39,3 +52,12 @@ def van_der_pol(v, t, mu):
     xd = y                             # x' = y
     yd = mu * (1 - x**2) * y - x       # y' = mu * (1 - x^2)*y - x
     return np.array([xd, yd])
+
+
+
+'''
+ToDo:
+    
+-Add Volterra-Lotka model,    
+
+'''
