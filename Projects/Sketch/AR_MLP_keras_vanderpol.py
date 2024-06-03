fd3ac190dd71133a1d06dfa24f4f46627c6a22df
Robin Schmidt <robin@rs-met.com>
2024-06-03 22:56:54 +0200

    Update AR_MLP_keras_vanderpol.py

--- Projects/Sketch/AR_MLP_keras_vanderpol.py
+++ Projects/Sketch/AR_MLP_keras_vanderpol.py
@@ -19,13 +19,14 @@ sys.path.append("../../Libraries")         # Make available for import
 
 # Imports from Keras:
 from keras.models     import Sequential
+from keras.layers     import Input
 from keras.layers     import Dense
-from keras.optimizers import RMSprop
+#from keras.optimizers import RMSprop
 
 # Imports from my own libraries:
 from rs.dynsys     import van_der_pol            # To generate the input data
 from rs.datatools  import signal_ar_to_nn        # For data reformatting
-from rs.learntools import synthesize_skl_mlp     # Resynthesize via MLP model
+#from rs.learntools import synthesize_skl_mlp     # Resynthesize via MLP model
 
 #==============================================================================
 # Setup
@@ -37,4 +38,43 @@ mu      = 1.0            # Nonlinearity parameter
 x0      = 0.0            # Initial condition x(0)
 y0      = 1.0            # Initial condition y(0)
 dim     = 0              # Dimension to use as time series. 0 or 1 -> x or y
+
+# Modeling parameters:
+delays  = [1,2,3,4]
+
+#==============================================================================
+# Processing
+
+# Create signal:
+t  = np.linspace(0.0, t_max, in_len)   # Time axis    
+vt = odeint(van_der_pol,               # Solution to the ODE
+            [x0, y0], t, args=(mu,))
+s = vt[:, dim]                         # Select one dimension for time series
+
+# Create the model
+X, y = signal_ar_to_nn(s, delays)      # Extract data for modeling
+
+model = Sequential()
+model.add(Input(shape=(4,)))     # use len(delays)
+#model.add(Dense(64, kernel_initializer = 'normal', activation = 'relu',
+#                input_shape = (13,))) 
+model.add(Dense(10, activation = 'tanh')) 
+model.add(Dense(1))
+# This produces a warning!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
